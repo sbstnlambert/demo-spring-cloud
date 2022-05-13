26c0e951590b5e7f0f51b9a2c29170d32426ed2f
sbstnlambert <sbstn.lambert@gmail.com>
2022-05-13 16:19:53 +0200

    added gateway2 module + added banner to modules

---
+++ ApiGatewayV2/src/main/java/be/technifutur/gateway2/config/RouteLocatorConfig.java
@@ -0,0 +1,35 @@
+package be.technifutur.gateway2.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfig {
+
+    // /client/** -> client service
+    // /movie/** -> movie service
+//    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("to-client",
+                        r -> r.path("/client/**")
+                                .and()
+                                .method("GET")
+                                .filters(f ->
+                                        f.rewritePath("/client/(?<path>.*)", "/$\\{path}")
+                                                .addRequestParameter("my-param", "its-value")
+                                                .retry(3)
+                                )
+                                .uri("lb://client-service")
+                )
+                .route("to-movie",
+                        r -> r.path("/movie/**")
+                                .filters(f -> f.rewritePath("/movie/(?<path>.*)", "/$\\{path}"))
+                                .uri("lb://movie-service")
+                )
+                .build();
+    }
+
+}
